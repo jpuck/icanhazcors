6b669b9871a4f7f356a1f6519a99e835b57cd9ff
Jeff Puckett <jeff@jeffpuckett.com>
2017-12-09 03:41:38 +0000

    preserve headers

--- public/index.php
+++ public/index.php
@@ -1,14 +1,26 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-
 $url = ltrim($_SERVER['REQUEST_URI'], '/');
 
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 1);
+curl_setopt($ch, CURLOPT_HEADER, 1);
 
-curl_exec($ch);
+$response = curl_exec($ch);
+
+$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+$headers = substr($response, 0, $header_size);
+$body = substr($response, $header_size);
 
 curl_close($ch);
+
+foreach (explode("\r\n", $headers) as $header) {
+    header($header);
+}
+
+header('Access-Control-Allow-Origin: *');
+
+echo $body;
